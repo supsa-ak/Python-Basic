9b0a18a18934df9357f9b4e8e8bab8eb26fc005c
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-07-24 23:55:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+class Student:
+
+  def __init__(self, name, grade, age):
+    self.name = name
+    self.grade = grade
+    self.age = age
+
+Sarthak = Student('Sarthak',99,19)
+Prabhu = Student('Prabhu',99,10)
+
+print(Sarthak.name)
+print(Prabhu.name)
+
+print(Sarthak.grade)
+print(Prabhu.grade)
+
+print(Sarthak.age)
+print(Prabhu.age)
+
+print(type(Student))
+
+
+
+
