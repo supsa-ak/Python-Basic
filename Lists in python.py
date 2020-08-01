89f126decbf244478b7a6d697eea866e3966c613
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-01 16:59:52 +0530

    Create Lists in python.py

---
+++ Lists in python.py
@@ -0,0 +1,55 @@
+#l=[1,2,3,4]
+#print(l)
+ 
+#l2=[1,'string',4.2,True,[1,2,3]]
+#print(l2)
+
+#print(l2[0])
+#print(l2[1])
+#print(l2[2])
+#print(l2[3])
+#print(l2[4])
+
+
+"""Below Functions Update List completely comment after execution"""
+
+#names=['sak','sarthak','brro']
+#print(names)
+
+#names.append('gary')
+#print(names)
+
+#names.insert(0,'gary')
+#print(names)
+
+#names.insert(1,'gary')
+#print(names)
+
+#names.remove("sak")
+#print(names)
+
+#names.remove('sarthak')
+#print(names)
+
+#names.reverse()
+#print(names)
+
+numbers=[6,4,2,9,12]
+print(numbers)
+
+#numbers.sort()
+#print(numbers)
+
+#I am making decending order
+
+#numbers.sort()
+#numbers.reverse()
+#print(numbers)
+
+'''This for loop is for iteration'''
+
+#for number in numbers:
+#  print(number)
+
+for i in numbers:
+  print(i)
