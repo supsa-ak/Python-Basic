fda836abaed5223603f55a3d01d4cb49647c0c6b
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-04 10:24:24 +0530

    Create Split and join function.py

---
+++ Split and join function.py
@@ -0,0 +1,39 @@
+           #problems = 'broke, pale, short, nerdy'
+#print(problems)
+
+#s="sarthak"
+#print(s)
+ 
+ 
+#p='prabhu'
+#print(p)
+
+#print('sarthak')
+#print("prabhu")
+
+
+#I found casting of string to list
+#p = list(problems)
+#print(p)
+
+
+
+#l=problems.split(', ')
+#print(l)
+
+
+#l=problems.split()
+#print(l)
+
+s = 'sak is handsome'
+l = s.split()
+print(l)
+
+#joined = ' and '.join(l)
+#print(joined)
+#print(type(joined))
+
+#csv = comma separated value file
+#csv = ','.join(l)
+#print(csv)
+#print(type(csv))
