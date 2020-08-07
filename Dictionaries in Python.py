4cbdd1a357adf39f4d77475ee3d4f36aadde96b7
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-07 15:11:08 +0530

    Create Dictionaries in Python.py

---
+++ Dictionaries in Python.py
@@ -0,0 +1,74 @@
+  #groceries = {'bananas':5,'oranges':3}
+#print(groceries['bananas'])
+
+
+#Gives error
+#print(groceris['hello'])
+
+#print(groceries.get('hello'))
+
+
+
+#contacts = {'Joe':'123-457','Jone':'923-852'}
+#rint(contacts['Joe'])
+
+
+
+
+#contacts = {'Joe':{'phone':'123-457','email':'Joe.com'},'Jone':['923-852','Jone.com']}
+   
+    
+#print(contats['Joe'])
+
+
+
+
+# METHODS IN DICTIONARTIES
+
+sentence = "I like the name Sak because the name Sak is the best"
+
+word_counts = {'I':1,
+              'like':1,
+              'the':3
+              }
+                
+
+#print(word_counts)
+#print(word_counts.items())
+#print(list(word_counts.items()))
+
+#print(word_counts.keys())
+#print(list(word_counts.keys()))
+
+#print(word_counts.values())
+#print(list(word_counts.values()))
+
+
+#Gives Error
+
+#print(word_counts)
+#word_counts.pop()
+#print(word_counts.pop())
+#print(word_counts)
+
+
+
+#print(word_counts)
+#word_counts.pop('I')
+#print(word_counts.pop('the'))
+#print(word_counts)
+
+#print(word_counts)
+#word_counts.popitem()
+#print(word_counts.popitem())
+#print(word_counts)
+
+#print(word_counts)
+#word_counts['Sak'] = 2
+#print(word_counts)
+
+
+print(word_counts)
+#word_counts.clear()
+#print(word_counts)
+v
