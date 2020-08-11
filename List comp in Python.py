b0a26dd3eb5da88cf13f3057c7dfaa7eb9fd2ca4
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-11 07:41:05 +0530

    Create List comp in Python.py

---
+++ List comp in Python.py
@@ -0,0 +1,60 @@
+#names = ['prabhu','sarthak','sak','pak']
+
+
+#put into new list
+#l = []
+#for person in names:
+#  l.append(person)
+#print(l)
+
+#print([person for person in names])
+
+
+
+
+
+
+
+#names = ['prabhu','sarthak','sak','pak']
+#l = []
+#for person in names:
+#  l.append(person + ' dumped me')
+#print(l)  
+  
+  
+#print([person + ' dumped me' for person in names])  
+ 
+ 
+ 
+ 
+ 
+#Challenge by Arron   
+#numbers = [1,2,3,4]  
+  
+#multiply numbers by 2  
+#num = []  
+#for no in numbers:
+#  num.append(no * 2)
+  
+#print(num)  
+
+
+
+
+
+
+movies_and_ratings = {"Interstellar":9,"Dark Knight":8,"50 shades":3,"50 shades darker":2}  
+l = [] 
+  
+#for movie in movies_and_ratings:
+#  if movies_and_ratings[movie]>6:
+#    l.append(movie)
+#print(l)
+  
+#print([movie for movie in movies_and_ratings if  movies_and_ratings[movie]>6])  
+  
+#Gives Error  
+if movies_and_ratings[9]>6:
+  print("ya")
+else:
+  print("no")
